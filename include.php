57c152025597e7090c8fb61f56fc0465a2475c5d
xujianguo <450846733@qq.com>
2017-09-12 16:56:06 +0800

    init repository

---
+++ include.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'qcloudapi-sdk-php-master' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'QcloudApi' . DIRECTORY_SEPARATOR. 'QcloudApi.php');
+require_once(__DIR__ . DIRECTORY_SEPARATOR . 'cos-php-sdk-v5-master' . DIRECTORY_SEPARATOR . 'cos-autoloader.php');
+require_once('conf.php');
